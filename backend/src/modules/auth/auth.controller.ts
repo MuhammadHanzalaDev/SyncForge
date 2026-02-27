cecc3cb72c8d454dbd57bffee0554751332cdbb4
MuhammadHanzalaDev <malikhanzalaasif@gmail.com>
2026-02-27 17:01:00 -0600

    setup test signup api

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,5 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import prisma from "@/config/prisma";
+import { Prisma } from "@prisma/client";
 
-const signUp = async (request: FastifyRequest, reply: FastifyReply) => {};
+type SignUpRequest = FastifyRequest<{
+  Body: Prisma.UserCreateInput;
+}>;
+
+const signUp = async (request: SignUpRequest, reply: FastifyReply) => {
+  const data = request.body;
+
+  const user = prisma.user.create({
+    data,
+  });
+
+  console.log("user", user);
+
+  return { user, message: "User Created Successfully!" };
+};
 
 export { signUp };
