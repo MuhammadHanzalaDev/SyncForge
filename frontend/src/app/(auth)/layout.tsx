fe8ec4f3cd6b445f65e62f34bad905ae29710f1d
MuhammadHanzalaDev <malikhanzalaasif@gmail.com>
2026-03-08 03:29:18 +0500

    feat: simplify login and signup pages by removing unnecessary layout and image components

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Layout({ children }: { children: React.ReactElement }) {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex justify-center items-center mb-8 mr-4">
+          <Image
+            className="dark:invert"
+            src="/SyncForge-transparent.png"
+            alt="Next.js logo"
+            width={190}
+            height={0}
+            priority
+          />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-
-import Image from "next/image";
 import { SignupForm } from "@/modules/auth/components/signup-form";
-import VerifyOtp from "@/modules/auth/components/verify-otp";
 import { useState } from "react";
 
 export default function Page() {
@@ -10,27 +7,6 @@ export default function Page() {
   const [email, setEmail] = useState("");
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <div className="flex justify-center items-center mb-8 mr-4">
-          <Image
-            className="dark:invert"
-            src="/SyncForge-transparent.png"
-            alt="Next.js logo"
-            width={190}
-            height={0}
-            priority
-          />
-        </div>
-        {isVerifyScreen ? (
-          <VerifyOtp email={email} />
-        ) : (
-          <SignupForm
-            setIsVerifyScreen={setIsVerifyScreen}
-            setEmail={setEmail}
-          />
-        )}
-      </div>
-    </div>
+    <SignupForm setIsVerifyScreen={setIsVerifyScreen} setEmail={setEmail} />
   );
 }
